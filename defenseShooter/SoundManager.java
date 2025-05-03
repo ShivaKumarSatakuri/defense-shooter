7a84f6fe6c0cbe9210e3750c96ca6b925482fbb0
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-05-03 23:35:21 +0530

    Added SoundManager class

--- defenseShooter/SoundManager.java
+++ defenseShooter/SoundManager.java
@@ -1,4 +1,23 @@
 package defenseShooter;
 
-public class SoundManager {
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+class SoundManager {
+
+    private SoundManager() {
+    }
+
+    public static void playSound(String soundFileName) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundFileName).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Error playing sound: " + e.getMessage());
+        }
+    }
 }
